75b2d354c3f77704e4e1e2f4ac3bdd9f5d21313d
Stefan <stefanabikaram@gmail.com>
2018-05-29 16:14:35 -0400

    added decompressor script

---
+++ ml/cnn.py
@@ -0,0 +1,14 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Input, Dense, Activation, Conv2D, MaxPooling2D,Flatten,Dropout
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+print(keras.__version__)
+
+np.random.seed(123)  # for reproducibility
+
+model = Sequential()
+model.add(Conv2D(32, (8, 8), activation='relu', input_shape=(224, 224, 3)))
---
+++ radar-data-matcher-downloader/netcdf_converter.py
@@ -0,0 +1,6 @@
+netcdf_tools_jar_file_path = ""
+dataset_path = ""
+
+command = "java -classpath toolsUI-4.0.jar ucar.nc2.FileWriter -in $in -out $out"
+
+
