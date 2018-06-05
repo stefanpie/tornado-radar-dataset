8df4f187a4e2e93106587b383cd50e2e94edee14
Stefan Abi-Karam <stefanabikaram@gmail.com>
2018-06-04 23:26:46 -0400

    Figured out how to graph velocity data at different elevations using matplotlib

--- ml/data-input-formatter.py
+++ ml/data-input-formatter.py
@@ -20,27 +20,35 @@ print(f.variables['numRadialsV'])
 print(f.variables['timeV'])
 print(f.variables['RadialVelocity'])
 
-for x in range(len(f.variables['RadialVelocity'])):
+station_id = getattr(f, 'Station')
+station_name = getattr(f, 'StationName')
+time_start = getattr(f, 'time_coverage_start')
+
+for x in range(f.dimensions['scanV'].size):
+    # TODO figure out how to calculate the right angle of elevation from netcdf file
+    angle = np.median(f.variables['elevationV'][x])
+
     lowest_angle = f.variables['RadialVelocity'][x]
-
     lowest_angle = np.array(lowest_angle)
-    print(lowest_angle)
-
+    lowest_angle[lowest_angle == 0] = np.nan
+    lowest_angle[lowest_angle == 1] = np.nan
+    # lowest_angle = (lowest_angle + f.variables['RadialVelocity'].add_offset) * f.variables['RadialVelocity'].scale_factor
     lowest_angle = lowest_angle.transpose()
 
-    lowest_angle[lowest_angle == 0] = np.nan
-    lowest_angle[lowest_angle == 1] = np.nan
+    gates = np.array(f.variables['distanceV'])
+    # angles = np.linspace(0, f.dimensions['radialV'].size-1, f.dimensions['radialV'].size)
+    angles = np.array(f.variables['azimuthV'][x])
+    min_index = np.argmin(angles)
 
-    gates = f.variables['distanceV']
-    angles = np.linspace(0, 360, 360)
-    # angles = f.variables['azimuthV'][x]
+    angles = np.roll(angles, min_index * -1)
+    lowest_angle = np.roll(lowest_angle, min_index * -1, axis=1)
 
-    lowest_angle[lowest_angle == 0] = np.nan
-    lowest_angle[lowest_angle == 1] = np.nan
+    fig, ax = plt.subplots()
+    im = ax.pcolormesh(angles, gates, lowest_angle)  # if you want contour plot
+    ax.set_title(station_id + " / " + station_name + " / " + time_start + " / " + "{:.4f}".format(angle) + " degrees")
+    ax.set_xlabel('Azimuth angle in degrees: 0 = true north, 90 = east')
+    ax.set_ylabel('Distance from radar in meters')
+    bar = fig.colorbar(im, ticks=range(-35, 40, 5), orientation='horizontal')
+    bar.set_label('Radial velocity in m/s')
 
-    r, theta = np.meshgrid(gates, angles)
-    print(theta)
-
-    plt.pcolormesh(angles, gates, lowest_angle, polar=True)
-    plt.colorbar()  # need a color bar to show the intensity scale
-    plt.show()  # boom
+    fig.show()
