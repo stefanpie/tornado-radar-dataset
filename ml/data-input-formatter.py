ca2ccd704a7c5ce74371ba35b59362a9adc19041
Stefan <stefanabikaram@gmail.com>
2018-06-06 00:10:23 -0400

    exteded script to show different data

--- ml/data-input-formatter.py
+++ ml/data-input-formatter.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import netCDF4
 import numpy as np
-from scipy import stats
+import math
+from scipy import signal
 
 f = netCDF4.Dataset('test1.nc', 'r')
 print(f.data_model)
@@ -25,41 +26,125 @@ station_id = getattr(f, 'Station')
 station_name = getattr(f, 'StationName')
 time_start = getattr(f, 'time_coverage_start')
 
-for x in range(f.dimensions['scanV'].size):
-    # TODO figure out how to calculate the right angle of elevation from netcdf file
-    angle = f.variables['elevationV'][x]
-    #angle = stats.mode((f.variables['elevationV'][x]), axis=None)[0][0]
-    angle = np.mean(angle)
-    print(angle)
-    angle = round(angle, 2)
-    print(angle)
+for elevation in range(f.dimensions['scanV'].size):
+    elevation_angle = f.variables['elevationV'][elevation]
+    elevation_angle = np.mean(elevation_angle)
+    rounded_elevation_angle = round(elevation_angle, 2)
 
-    lowest_angle = f.variables['RadialVelocity'][x]
-    lowest_angle = np.array(lowest_angle)
-    lowest_angle[lowest_angle == 0] = np.nan
-    lowest_angle[lowest_angle == 1] = np.nan
+    spectrum_width_at_elevation = f.variables['RadialVelocity'][elevation]
+    spectrum_width_at_elevation = np.array(spectrum_width_at_elevation)
+    spectrum_width_at_elevation[spectrum_width_at_elevation == 0] = np.nan
+    spectrum_width_at_elevation[spectrum_width_at_elevation == 1] = np.nan
     # lowest_angle = (lowest_angle + f.variables['RadialVelocity'].add_offset) * f.variables['RadialVelocity'].scale_factor
-    lowest_angle = lowest_angle / f.variables['RadialVelocity'].scale_factor
-    lowest_angle = lowest_angle.transpose()
+    spectrum_width_at_elevation = spectrum_width_at_elevation / f.variables['RadialVelocity'].scale_factor
+    spectrum_width_at_elevation = spectrum_width_at_elevation.transpose()
 
     gates = np.array(f.variables['distanceV'])
-    # angles = np.linspace(0, f.dimensions['radialV'].size-1, f.dimensions['radialV'].size)
-    angles = np.array(f.variables['azimuthV'][x])
-    min_index = np.argmin(angles)
+    gates = gates / 1000
+    # for i in range(len(gates)):
+    #     gates[i] = gates[i] * math.cos(elevation_angle)
+    #     if gates[i] < 0:
+    #         gates[i] *= -1
 
+    angles = np.array(f.variables['azimuthV'][elevation])
+    min_index = np.argmin(angles)
     angles = np.roll(angles, min_index * -1)
-    lowest_angle = np.roll(lowest_angle, min_index * -1, axis=1)
+    spectrum_width_at_elevation = np.roll(spectrum_width_at_elevation, min_index * -1, axis=1)
 
     fig, ax = plt.subplots()
-    im = ax.pcolormesh(angles, gates, lowest_angle)  # if you want contour plot
-    ax.set_title(station_id + " / " + station_name + " / " + time_start + " / " + "{:.4f}".format(angle) + " degrees")
+    im = ax.pcolormesh(angles, gates, spectrum_width_at_elevation)  # if you want contour plot
+    ax.set_title(station_id + " / " + station_name + " / " + time_start + " / " + "{:.4f}".format(rounded_elevation_angle) + " degrees")
     ax.set_xlabel('Azimuth angle in degrees: 0 = true north, 90 = east')
-    ax.set_ylabel('Distance from radar in meters')
+    ax.set_ylabel('Distance from radar in km')
+    bar = fig.colorbar(im, ticks=range(-70, 75, 5), orientation='horizontal')
+    bar.set_label('Radial velocity in m/s')
+
+    #fig.show()
+
+for elevation in range(f.dimensions['scanV_HI'].size):
+    elevation_angle = f.variables['elevationV_HI'][elevation]
+    elevation_angle = np.mean(elevation_angle)
+    rounded_elevation_angle = round(elevation_angle, 2)
+
+    spectrum_width_at_elevation = f.variables['RadialVelocity_HI'][elevation]
+    spectrum_width_at_elevation = np.array(spectrum_width_at_elevation)
+    spectrum_width_at_elevation[spectrum_width_at_elevation == 0] = np.nan
+    spectrum_width_at_elevation[spectrum_width_at_elevation == 1] = np.nan
+    # lowest_angle = (lowest_angle + f.variables['RadialVelocity'].add_offset) * f.variables['RadialVelocity'].scale_factor
+    spectrum_width_at_elevation = spectrum_width_at_elevation / f.variables['RadialVelocity_HI'].scale_factor
+    spectrum_width_at_elevation = spectrum_width_at_elevation.transpose()
+
+    gates = np.array(f.variables['distanceV_HI'])
+    gates = gates / 1000
+
+    angles = np.array(f.variables['azimuthV_HI'][elevation])
+    min_index = np.argmin(angles)
+    angles = np.roll(angles, min_index * -1)
+    spectrum_width_at_elevation = np.roll(spectrum_width_at_elevation, min_index * -1, axis=1)
+
+    fig, ax = plt.subplots()
+    im = ax.pcolormesh(angles[:], gates[:], spectrum_width_at_elevation[:])  # if you want contour plot
+    title = station_id + " / " + station_name + " / " + time_start + " / " + "{:.2f}".format(rounded_elevation_angle) + " degrees" + " / HR"
+    ax.set_title(title)
+    ax.set_xlabel('Azimuth angle in degrees: 0 = true north, 90 = east')
+    ax.set_ylabel('Distance from radar in km')
+    bar = fig.colorbar(im, ticks=range(-70, 75, 5), orientation='horizontal')
+    bar.set_label('Radial velocity in m/s')
+    filename = title
+    for c in r'[]/\;,><&*:%=+@!#^()|?^':
+        filename = filename.replace(c, '')
+    fig.show()
+    fig.savefig(filename + ".png", dpi=2500)
+
+for elevation in range(f.dimensions['scanV_HI'].size):
+    elevation_angle = f.variables['elevationV_HI'][elevation]
+    elevation_angle = np.mean(elevation_angle)
+    rounded_elevation_angle = round(elevation_angle, 2)
+
+    spectrum_width_at_elevation = f.variables['SpectrumWidth_HI'][elevation]
+    spectrum_width_at_elevation = np.array(spectrum_width_at_elevation)
+    spectrum_width_at_elevation[spectrum_width_at_elevation == 0] = np.nan
+    spectrum_width_at_elevation[spectrum_width_at_elevation == 1] = np.nan
+    # lowest_angle = (lowest_angle + f.variables['RadialVelocity'].add_offset) * f.variables['RadialVelocity'].scale_factor
+    spectrum_width_at_elevation = spectrum_width_at_elevation / f.variables['SpectrumWidth_HI'].scale_factor
+    spectrum_width_at_elevation = spectrum_width_at_elevation.transpose()
+
+
+
+    gates = np.array(f.variables['distanceV_HI'])
+    gates = gates / 1000
+
+    angles = np.array(f.variables['azimuthV_HI'][elevation])
+    min_index = np.argmin(angles)
+    angles = np.roll(angles, min_index * -1)
+    spectrum_width_at_elevation = np.roll(spectrum_width_at_elevation, min_index * -1, axis=1)
+
+
+    fig, ax = plt.subplots()
+    im = ax.pcolormesh(angles[:], gates[:], spectrum_width_at_elevation[:])  # if you want contour plot
+    ax.set_title(station_id + " / " + station_name + " / " + time_start + " / " + "{:.2f}".format(rounded_elevation_angle) + " degrees" + " / HR")
+    ax.set_xlabel('Azimuth angle in degrees: 0 = true north, 90 = east')
+    ax.set_ylabel('Distance from radar in km')
     bar = fig.colorbar(im, ticks=range(-70, 75, 5), orientation='horizontal')
     bar.set_label('Radial velocity in m/s')
 
     fig.show()
 
-print(np.array(f.variables['azimuthV'][0].size))
-for x in np.array(f.variables['azimuthV'][0].size):
-    pass
+
+# elevation_angle = 0
+#
+# data_at_elevations =  []
+# for x in range(f.dimensions['scanV'].size):
+#     data_at_elevation = np.array(f.variables['RadialVelocity'][x][1])
+#     data_at_elevations.append(np.array(data_at_elevation))
+# data_at_elevations = np.array(data_at_elevations)
+#
+# elevation_angles = []
+# for elevation in range(f.dimensions['scanV'].size):
+#     elevation_angle = f.variables['elevationV'][elevation]
+#     #angle = stats.mode((f.variables['elevationV'][x]), axis=None)[0][0]
+#     elevation_angle = np.mean(elevation_angle)
+#     elevation_angle = round(elevation_angle, 2)
+#     elevation_angles.append(elevation_angle)
+# elevation_angles = np.array(elevation_angles)
+
